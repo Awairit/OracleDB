938f7612bb41e69fc1ad14a9ed139b07dba17a0e
Mohammad Irshad <awairit2k20@gmail.com>
2025-12-16 15:29:37 +0530

    Just about steps for PDBC(Python Datbase Communication)

--- OracleDB/oracledb_ex_01.py
+++ OracleDB/oracledb_ex_01.py
@@ -1,2 +1,14 @@
+''''   Software and Database Software. To do this we need to learn the following the Steps.
+****************************************************************************************************
+Step-1: import Appropriate Data Base Module and Other Modules if required.
+Step-2: Every Python Program Must Obtain the Connection from Database Software
+Step-3: Every Python Program Must  Create an object of Cursor
+Step-4: Every Python Program Must Design the Query ,Place the Query in Cursor object and send for Executing in
+               Database.
+Step-5: Every Python Program Must Process the Result of the Query which is coming from Data Base through Cursor
+             object
+Step-6: Every Python Program is Recommned to Close the Connection from Database Software (Optional )
+****************************************************************************************************'''
+
 import oracledb
 print(oracledb.__version__)
